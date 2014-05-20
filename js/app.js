8c8545293fe9e1677e795765c96eb509b8d9afa6
ajc289 <ubuntu@ip-10-154-193-80.ec2.internal>
2014-05-20 13:42:55 -0400

    slight ui improvements

--- js/app.js
+++ js/app.js
@@ -3,11 +3,8 @@ var phonecatApp = angular.module('phonecatApp', ['ui.bootstrap','ngRoute','phone
 phonecatApp.config(['$routeProvider',
    function ($routeProvider){
       $routeProvider.
-         when('/search_test',{templateUrl: 'partials/search_test.html', controller: 'SearchTestCtrl'}).
-         when('/search_test/:query',{templateUrl: 'partials/search_result.html', controller: 'SearchResultCtrl'}).
-         when('/reviews',{templateUrl: 'partials/review-list.html', controller: 'ReviewListCtrl'}).
+         when('/search',{templateUrl: 'partials/search.html', controller: 'SearchCtrl'}).
+         when('/search/:query',{templateUrl: 'partials/search_result.html', controller: 'SearchResultCtrl'}).
          when('/reviews/:product_code',{templateUrl: 'partials/review-detail.html', controller: 'ReviewDetailCtrl'}).
-         when('/phones',{templateUrl: 'partials/phone-list.html', controller: 'PhoneListCtrl'}).
-         when('/phones/:phoneId',{templateUrl: 'partials/phone-detail.html', controller: 'PhoneDetailCtrl'}).
-         otherwise({redirectTo: '/phones'});
+         otherwise({redirectTo: '/search'});
       }]);
--- js/controllers.js
+++ js/controllers.js
@@ -1,21 +1,16 @@
 var phonecatControllers = angular.module('phonecatControllers', []);
  
-phonecatControllers.controller('ReviewListCtrl', ['$scope', '$http', function ($scope, $http) {
-$http.get('json/res2.json').success(function(data){
-$scope.reviews = data;
-});
 
-}]);
-
-phonecatControllers.controller('SearchTestCtrl', ['$scope', '$http', function ($scope, $http) {
+phonecatControllers.controller('SearchCtrl', ['$scope', '$http', function ($scope, $http) {
 
 $scope.query = '';
 
   var i =0;
-  var images = ['http://www.sparkawards.com/galleries/archive/Butternut_Mato_ID_FrontRightOpen_LinenWhite_IFContest.jpg','http://images.amazon.com/images/G/01/electronics/sony/sony-12q4-Eseries-14-white-cover-lg.jpg','http://images.amazon.com/images/G/01/electronics/sony/sony-12q4-Eseries-11-white-duo-lg.jpg'];
+  //var images = ['http://www.sparkawards.com/galleries/archive/Butternut_Mato_ID_FrontRightOpen_LinenWhite_IFContest.jpg','http://images.amazon.com/images/G/01/electronics/sony/sony-12q4-Eseries-14-white-cover-lg.jpg','http://images.amazon.com/images/G/01/electronics/sony/sony-12q4-Eseries-11-white-duo-lg.jpg'];
+  var images = ['http://1.bp.blogspot.com/-ZSiPdR0LOJg/UIgCKcmq6LI/AAAAAAAAA7Q/ghmOmoMAJPY/s1600/DSCN8690.JPG','http://images.amazon.com/images/G/01/electronics/sony/sony-12q4-Eseries-14-white-cover-lg.jpg','http://rekkerd.org/img/201301/akai_mpc_earbuds.jpg'];
   var image = $('#slideit');
 
-  image.css('background-image', 'url(http://www.sparkawards.com/galleries/archive/Butternut_Mato_ID_FrontRightOpen_LinenWhite_IFContest.jpg)');
+  image.css('background-image', 'url(http://rekkerd.org/img/201301/akai_mpc_earbuds.jpg)');
 
   setInterval(function(){
    image.css('background-image', 'url(' + images [i++] +')');
@@ -25,7 +20,8 @@ $scope.query = '';
   }, 5000);
 
 $scope.new_search_query = function(){
-   document.getElementById('search_form').action = 'http://www.igniipotent.com/reviews/angular-reviews/app/index.html?#/search_test/' + $scope.query.replace(/ /g, ';');
+   //document.getElementById('search_form').action = 'http://www.igniipotent.com/reviews/angular-reviews/app/index.html?#/search/' + $scope.query.replace(/ /g, ';');
+   document.getElementById('search_form').action = 'http://www.igniipotent.com/#/search/' + $scope.query.replace(/ /g, ';');
    document.getElementById('search_form').submit();
 }
 }]);
@@ -33,15 +29,31 @@ $scope.new_search_query = function(){
 phonecatControllers.controller('SearchResultCtrl', ['$scope', '$routeParams', '$http',
 function($scope, $routeParams, $http) {
 
-   $http.get('/search?query=' + $routeParams.query).success(function(data){
-   $scope.search_results = data;
+      $http.get('/search?query=' + $routeParams.query).success(function(data){
+      $scope.search_results = data;
+      console.log(data['result'].length);
+      $scope.search_results['number'] = 'Your search returned ' + $scope.search_results['result'].length + ' results';
+      
+      /*if ($scope.search_results['result'].length == 0)
+      {
+         $scope.search_results['none'] = 'Your search returned 0 results';
+      }
+      else
+      {
+         $scope.search_results['none'] = 'Your search returned 0 results';
+      }*/
    });
 
+$scope.open_in_tab = function(){
+   var win = window.open($scope.review['page_url'], '_blank');
+   win.focus();
+};
 
 $scope.new_search_query = function(){
-   document.getElementById('search_form').action = 'http://www.igniipotent.com/reviews/angular-reviews/app/index.html?#/search_test/' + $scope.query.replace(/ /g, ';');
+   //document.getElementById('search_form').action = 'http://www.igniipotent.com/reviews/angular-reviews/app/index.html?#/search/' + $scope.query.replace(/ /g, ';');
+   document.getElementById('search_form').action = 'http://www.igniipotent.com/#/search/' + $scope.query.replace(/ /g, ';');
    document.getElementById('search_form').submit();
-}
+};
 }]);
 
 phonecatControllers.controller('ReviewDetailCtrl', ['$scope', '$routeParams', '$http', '$sce',
@@ -53,6 +65,25 @@ feature_list = $scope.review['features'];
 
 for (var i = 0; i < feature_list.length; i++)
 {
+   if (i > 0)
+   {
+      feature_list[i]['accordion_class'] = "panel-collapse collapse";
+   }
+   else
+   {
+      feature_list[i]['accordion_class'] = "panel-collapse collapse in";
+   }
+   feature_list[i]['dec_class'] = "btn btn-default disabled";
+
+   if (feature_list[i]['feature_text'].length > 1)
+   {
+      feature_list[i]['inc_class'] = "btn btn-default";
+   }
+   else
+   {
+      feature_list[i]['inc_class'] = "btn btn-default disabled";
+   }
+
    var new_html = "<span style=\"background-color:yellow;\">" + feature_list[i]['feature_name'] + "</span>";
    var re = new RegExp (feature_list[i]['feature_name'], 'g');
    for (var j = 0; j < feature_list[i]['feature_text'].length; j++)
@@ -65,16 +96,19 @@ for (var i = 0; i < feature_list.length; i++)
 
 });
 
+
+$scope.new_search_query = function(){
+   document.getElementById('search_form').action = 'http://www.igniipotent.com/reviews/angular-reviews/app/index.html?#/search_test/' + $scope.query.replace(/ /g, ';');
+   document.getElementById('search_form').submit();
+};
+
 $scope.open_in_tab = function(){
    var win = window.open($scope.review['page_url'], '_blank');
    win.focus();
 };
 
-
 $scope.to_trusted = function(html){
-
    return $sce.trustAsHtml (html);
-
 };
 
 $scope.inc_curr_text = function(feature_name){
@@ -90,6 +124,12 @@ for (var i = 0; i < feature_list.length; i++)
          feature_list[i]['feature_curr_text_index'] += 1;
          var curr_index = feature_list[i]['feature_curr_text_index'];
          feature_list[i]['feature_curr_text'] = feature_list[i]['feature_text'][curr_index];
+         feature_list[i]['dec_class'] = "btn btn-default";
+      }
+      //else
+      if (feature_list[i]['feature_curr_text_index'] + 1 >= feature_list[i]['feature_text'].length)
+      {
+         feature_list[i]['inc_class'] = "btn btn-default disabled";
       }
    }
 }
@@ -109,6 +149,12 @@ for (var i = 0; i < feature_list.length; i++)
          feature_list[i]['feature_curr_text_index'] -= 1;
          var curr_index = feature_list[i]['feature_curr_text_index'];
          feature_list[i]['feature_curr_text'] = feature_list[i]['feature_text'][curr_index];
+         feature_list[i]['inc_class'] = "btn btn-default";
+      }
+      //else
+      if (feature_list[i]['feature_curr_text_index'] - 1 < 0)
+      {
+         feature_list[i]['dec_class'] = "btn btn-default disabled";
       }
    }
 }
